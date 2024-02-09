76a43bd303eb00217c60536fe5a9dabd8c4a4e2d
Gaurav <mahendrakergaurav@gmail.com>
2024-02-09 21:11:35 +0530

    background.js taking periodic screenshots and posting to backend

--- app/src/background.js
+++ app/src/background.js
@@ -1,31 +1,42 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+const endpoint = 'http://127.0.0.1:5000';
 
-const endpoint = 'http://127.0.0.1:5000'
+function takeAndSendScreenshot() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+    chrome.tabs.captureVisibleTab(
+      null,
+      { format: 'png' },
+      function (dataUrl) {
+        console.log(dataUrl);
+        fetch(endpoint + '/screenshot', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ screenshotData: dataUrl }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              console.log('Screenshot sent successfully');
+            } else {
+              console.error('Error sending screenshot:', response.statusText);
+            }
+          })
+          .catch((error) => {
+            console.error('Error sending screenshot:', error);
+          });
+      }
+    );
+  });
+}
 
-    fetch(endpoint + '/html', {
-      method: "POST",
-      headers : {
-        "Content-Type": "application/json"
-      },
-      body : JSON.stringify(request.payload.message)
-    }).then((response) => {console.log(response);})
-      .catch((error) => console.error("Error:", error));
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
-});
+// Function to schedule taking and sending screenshots every 5 seconds
+function scheduleScreenshot() {
+
+  setInterval(takeAndSendScreenshot, 500); // 5000 milliseconds = 5 seconds
+}
+
+// Call the function to start scheduling screenshots
+scheduleScreenshot();
