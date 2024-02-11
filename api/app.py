caa3cdd2059bb0e6cb0711e423a5491720e66461
Rupal17shah <shah17rupal@gmail.com>
2024-02-11 14:57:51 +0530

    my changes

--- api/app.py
+++ api/app.py
@@ -4,35 +4,24 @@ import torch
 from sentence_transformers import SentenceTransformer
 from model import Classifier, skipblock, predlist
 from rag import get_dark_patterns
-import imgkit
 import time
+start = time.time()
 dpdet = Classifier()
 embed_model  = SentenceTransformer('BAAI/bge-large-en')
 dpdet.load_state_dict(torch.load('model.bin'))
+print('Time taken to load model:', time.time()-start)
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/html', methods = ['POST'])
-def returnHTML():
-    data = request.json
-    imgkit.from_string(data, 'out.jpg')
-    # print(data)
-    return 'successfully recieved'
-
 
 @app.route('/dom', methods = ['POST'])
 def returnDOM():
     data = request.json.split('\n')
-    # print(data)
     st=time.time()
-    det = predlist(dpdet, embed_model,data)
-    dark = []
+    det = predlist(dpdet, embed_model, data)
+    dark = {}
     for i in det:
-        dark.append({
-            "text": i[0],
-            "label": get_dark_patterns(i[1])[0][-1]
-        })
-    print(dark)
+        dark[i[0]] = get_dark_patterns(i[1])[0][-1]
+    print('Time taken:',time.time()-st)
     return jsonify(dark)
 
 
