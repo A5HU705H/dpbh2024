c4b975ca5e5561aa3c8ced10ae4e967c12ca6186
Samip Shah <95608626+snarkyidiot@users.noreply.github.com>
2024-02-11 16:05:18 +0530

    WebSocket

--- app/src/background.js
+++ app/src/background.js
@@ -4,34 +4,42 @@
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
-
+const ws = new WebSocket("ws://localhost:8000/ws");
 const endpoint = 'http://127.0.0.1:5000';
-console.log('Background is running');
-let tab;
-chrome.tabs.query({ active: true, currentWindow: true }).then(([activeTab])=>{
-  tab=activeTab;
-  console.log(tab);
-});
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('message received');
-  if (request.type === 'innerText') {
-    sendResponse({type:'innerText'});
-    async function fetchRes() {
-      const res = await fetch(endpoint + '/dom', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-        body: JSON.stringify(request.payload.text),
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .catch((error) => console.error('Error:', error));
-        chrome.tabs.sendMessage(tab.id, { type: 'innerText', payload: res });
-    }
-    fetchRes();
-    return true;
-  }
-});
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   console.log('message received');
+//   if (request.type === 'innerText') {
+//     sendResponse({type:'innerText'});
+//     async function fetchRes() {
+//       const res = await fetch(endpoint + '/dom', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           Accept: 'application/json',
+//         },
+//         body: JSON.stringify(request.payload.text),
+//       })
+//         .then((response) => {
+//           return response.json();
+//         })
+//         .catch((error) => console.error('Error:', error));
+//         chrome.tabs.sendMessage(sender.tab.id, { type: 'innerText', payload: res });
+//     }
+//     fetchRes();
+//     return true;
+//   }
+// });
+function takeAndSendScreenshot() {
+    chrome.tabs.captureVisibleTab(
+      null,
+      { format: 'png' },
+      function (dataUrl) {
+        console.log(dataUrl,"dataUrl: ");
+        ws.send(dataUrl);
+      }
+    );
+}
+
+// Function to schedule taking and sending screenshots every 5 seconds
+
+setInterval(takeAndSendScreenshot, 550); // 5000 milliseconds = 5 seconds
